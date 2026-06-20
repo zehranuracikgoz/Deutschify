9f80e7a60579ce5ab6f3d2fb56ee599310122235
Zehra Nur <zehra.acikgoz@outlook.com>
2026-06-20 22:51:54 +0300

    test: yerel postgresql test ortamına yönlendirildi

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'), encoding='utf-8-sig')
+os.environ['DATABASE_URL'] = 'postgresql://postgres:postgres@localhost:5433/deutschify'
 
 import pytest
 import psycopg2
